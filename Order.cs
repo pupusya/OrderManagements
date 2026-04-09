80f0e7db67e3f85c51193015c40c6b5c8e629983
Кудрявцева Мария Сергеевна <232909072091-12@A301WS4>
2026-04-09 13:27:02 +0300

    Создан класс Order

---
+++ Order.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public enum OrderStatus
+{
+    Новый,
+    В_обработке,
+    Завершён
+}
+
+namespace OrderManagements
+{
+    public class Order
+    {
+        public string CustomerName { get; set; }
+        public string Description { get; set; }
+        public OrderStatus Status { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public Order(string customerName, string description, DateTime creationDate)
+        {
+            CustomerName = customerName;
+            Description = description;
+            Status = OrderStatus.Новый;
+            CreationDate = creationDate;
+        }
+
+        public void UpdateStatus(OrderStatus newStatus)
+        {
+            Status = newStatus;
+        }
+    }
+}
